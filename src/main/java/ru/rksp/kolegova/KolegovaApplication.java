59702bdc3b8f68e0c8556ad6b69c884bbc878be1
juliakolegova <julia.im.ginger@gmail.com>
2025-10-25 20:19:56 +0300

    первый коммит для client-server-lab-01

---
+++ src/main/java/ru/rksp/kolegova/KolegovaApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.kolegova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolegovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolegovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/kolegova/KolegovaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.kolegova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolegovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
